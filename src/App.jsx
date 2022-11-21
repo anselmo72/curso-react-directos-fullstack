076c419f9228337231d52dbd8565221a36f394d7
anselmo72 <contacto@gmail.com>
2022-11-21 17:51:26 +0000

    formulario parte 1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import FormNoControlado from './components/FormNoControlado'
+
+import Formulario from './components/Formulario'
+
+function App() {
+  return (
+    <div className='container'>
+      <h1>Formulários</h1>
+      {/* <FormNoControlado /> */}
+      <Formulario />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,106 @@
+import { useState } from 'react'
+
+const Formulario = () => {
+  const [todo, setTodo] = useState({
+    todoName: '',
+    todoDescripcion: '',
+    todoEstado: 'pendiente',
+    todoCheck: false,
+  })
+
+  const [error, setError] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const { todoName, todoDescripcion } = todo
+
+    if (!todoDescripcion.trim() || !todoName.trim()) {
+      setError(true)
+      return
+    }
+
+    setError(false)
+
+    console.log(todo)
+
+    setTodo({
+      todoName: '',
+      todoDescripcion: '',
+      todoEstado: 'pendiente',
+      todoCheck: false,
+    })
+  }
+
+  const handleChange = (e) => {
+    // console.log(e.target.value)
+    // console.log(e.target.name)
+
+    console.log(e.target.type)
+
+    const { name, value, checked, type } = e.target
+
+    setTodo({
+      ...todo,
+      [name]: type === 'checkbox' ? checked : value,
+    })
+  }
+
+  const PintarError = () => {
+    return <div className='alert alert-danger'>Campos obrigatórios</div>
+  }
+
+  return (
+    <>
+      <h2>No controlado</h2>
+
+      {error && <PintarError />}
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='Ingrese Todo'
+          name='todoName'
+          className='form-control mb-2'
+          onChange={handleChange}
+          value={todo.todoName}
+        />
+        <textarea
+          name='todoDescripcion'
+          className='form-control mb-2'
+          placeholder='ingrese descripcion del todo'
+          defaultValue='Descripcion Tarea #01'
+          onChange={handleChange}
+          value={todo.todoDescripcion}
+        ></textarea>
+        <select
+          name='todoEstado'
+          className='form-control mb-2'
+          onChange={handleChange}
+          value={todo.todoEstado}
+        >
+          <option value='pendiente'>Pendiente</option>
+          <option value='completada'>Completada</option>
+        </select>
+
+        <div className='form-check'>
+          <input
+            className='form-check-input'
+            type='checkbox'
+            onChange={handleChange}
+            checked={todo.todoCheck}
+            name='todoCheck'
+            id='flexCheckDefault'
+          />
+          <label className='form-check-label' htmlFor='flexCheckDefault'>
+            Dar prioridad
+          </label>
+        </div>
+
+        <button className='btn btn-primary' type='submit'>
+          Agregar
+        </button>
+      </form>
+    </>
+  )
+}
+export default Formulario
